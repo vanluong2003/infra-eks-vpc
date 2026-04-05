de34dc9a68c70ffa63fd10b05ab7efa2ff365824
vanluong2003 <adhotran2003@gmail.com>
2026-04-05 09:31:40 +0700

    update standard backend

--- providers.tf
+++ providers.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.25.0"
     }
   }
+  backend "s3" {
+    bucket = "vprofile-tfstate-bucket-001"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
