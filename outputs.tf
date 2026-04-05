b75de83752bc84e0a8323e45c8b689fce195985a
vanluong2003 <adhotran2003@gmail.com>
2026-04-05 21:41:48 +0700

    chore: remove trailing whitespace and unnecessary comments from outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,3 @@ output "configure_kubectl" {
   description = "Command to configure kubectl"
   value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}"
 }
-
-##
