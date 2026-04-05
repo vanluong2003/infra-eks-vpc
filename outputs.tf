1824cc7950add95c85bb07ca1fbb0b8c61c6ab28
vanluong2003 <adhotran2003@gmail.com>
2026-04-05 21:28:30 +0700

    feat: add GitHub Actions workflow for Terraform CI/CD and update outputs.tf formatting

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,5 @@ output "configure_kubectl" {
   description = "Command to configure kubectl"
   value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}"
 }
+
+##
